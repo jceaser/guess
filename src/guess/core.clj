5356f58c7d229299f335ce097c3f07b20e172208
Thomas Cherry <thomas.cherry@gmail.com>
2020-07-05 11:18:44 -0400

    got correct items and exact match working

--- src/guess/core.clj
+++ src/guess/core.clj
@@ -13,21 +13,69 @@
   (build-answer (dec add-count) (conj answer (rand-int 3)))
   answer)))
 
+(defn true-count
+  "counts the number of true values exist in the given list"
+  [list]
+  (get (frequencies list) true))
+
+(defn positions-match
+  "find how many positions match and return true if all match"
+  [answer round]
+  ;(def a (reduce #(or %1 (some? (some #{%2} answer))) false round))
+  ;(print "stub: check" round "for positions matching:")
+  (loop [x 0
+         v [] ]
+    (if (< x 4)
+      (recur (inc x) (conj v (= (get round x) (get answer x))))
+      (println "Exact Matches:" (true-count v)))))
+
+(defn contain-match
+  "util function to check a value in a list"
+  [list value]
+  (some #(= value %) list))
+
+(defn contain-matches
+  "checks the round against the answer and looks for any items that are correct"
+  [answer round]
+  (let [exact (true-count (map #(contain-match answer %) round))]
+    (println "Correct Items:" exact))
+  true
+  )
+
+(defn positions-match2
+ "report how many positions are the same"
+ ([answer round] (contain-matches answer round 0))
+ ([answer round index]
+ (println "stub: check" round "for matches in" answer "with" (nth answer index) "and" (nth round index) "using" index "which is below" (count answer))
+ (if (< index (-(count answer) 1))
+   (contain-matches answer round (inc index)))
+ (if (= (nth answer index) (nth round index)) (println " found") :nop )))
+
 (defn play-round
+ "Play one round, ask for input, then judge it against the answer
+ answer is a vector of numbers
+ return is a vector of numbers, the guess
+ "
  [answer]
  [1 2 3 4])
+
+(defn judge-rounds
+ "judge the last round and report, return true if game can continue"
+ [answer rounds]
+ (let [round (last rounds)]
+  (and (contain-matches answer round) (positions-match answer round))))
 
 (defn game-loop
  ""
  ([answer] (game-loop answer []))
  ([answer rounds]
+  ;(println "answer is:" answer)
   ;(println "past rounds" rounds)
   ;(println "round" round-count)
   (if (< (count rounds) 10)
    (do
-    (def r (conj rounds (play-round answer)))
-    (println r)
-    (game-loop answer r))
+    (if (judge-rounds answer (conj rounds (play-round answer)))
+     (game-loop answer rounds)))
    rounds))
 )
 
